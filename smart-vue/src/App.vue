80e8fc5f4686bb5f49ecd76076d3002373f0c3c0
raven <857697474@qq.com>
2020-04-11 00:59:18 +0800

    更新了前端页面

---
+++ smart-vue/src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="app">
+    <div class="head">
+        <div>
+          <ul>
+            <li><a href="#">首页</a></li>
+            <li><a href="#">时间安排</a></li>
+            <li><a href="#">设备管理</a></li>
+            <li><a href="#">我们的理念</a></li>
+          </ul>
+        </div>
+    </div>
+    <div class="content">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+*{
+  padding: 0px;
+  margin: 0px;
+}
+.head{
+  text-align: center;
+  background-color: transparent;
+  width: 100%;
+  position: fixed;
+  color: whitesmoke;
+  font-size: 16px;
+  font-weight: bold;
+  z-index: 10;
+  
+}
+.head ul::before{
+  content:url("./assets/ico.png");
+  position: relative;
+  left: -100px;
+  top:10px;
+}
+.head ul{
+  list-style: none;
+  display: flex;
+  flex-direction: row;
+  width: 60%;
+  justify-content: center;
+  margin-left: 150px;
+  
+  /* background-color: tomato; */
+}
+.head ul li{
+  width: 25%;
+  padding: 30px 10px ;
+  /* background-color:thistle; */
+  margin: 0 10px;
+ 
+}
+.head ul li a{
+  text-decoration: none;
+  color: aliceblue;
+  display: block;
+}
+</style>
---
+++ smart-vue/src/components/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div >
+    <img src="../assets/homebg.png" alt="" style="width:100%;">
+    <img src="../assets/app.png" alt="" class="app">
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: 'index',
+  mounted () {
+    window.addEventListener('scroll', this.handleScroll, true)
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    handleScroll() {
+        var heigth = document.documentElement.scrollTop;
+        var headDom = document.getElementsByClassName("head")[0];
+        var ls = document.getElementsByClassName("head")[0].childNodes[0].childNodes[0].children;
+        headDom.style.animation="mymove  0.5s linear"
+        if(heigth >= 100){
+          
+          headDom.style.backgroundColor ="whitesmoke";
+          headDom.style.borderBottom= '2px solid #e0e0e0';
+          for (let index = 0; index < ls.length; index++) {
+              ls[index].childNodes[0].style.color="#6F6F6F";
+          }
+        }
+        if(heigth == 0){
+          headDom.style.backgroundColor ="transparent";
+          headDom.style.borderBottom= '0';
+          for (let index = 0; index < ls.length; index++) {
+              ls[index].childNodes[0].style.color="white";
+          }
+        }
+        
+    }
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.app{
+  position: relative;
+  top: -600px;
+  left: 900px;
+  width: 400px;
+}
+@keyframes mymove{
+  from{
+    background-color: transparent;
+    transform: scale(1.3);
+  }to{
+    background-color: whitesmoke;
+  }
+}
+</style>
