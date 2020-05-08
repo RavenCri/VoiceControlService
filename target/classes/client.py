d1eb0ab9d3c3cec0fded9cf5691e1eeb46af4f88
raven <857697474@qq.com>
2020-05-09 00:21:46 +0800

    刪除target

--- target/classes/client.py
+++
@@ -1,9 +0,0 @@
-import socket
-import argparse
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--instruct", required=True,help="please input the instruct")
-args = vars(ap.parse_args())
-#创建一个客户端的socket对象
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(("192.168.1.100", 8080))
-client.send(args['instruct'].encode("utf-8"))
